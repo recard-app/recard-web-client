368d449956d69dd7cfe208623d93a6df76b1727e
Evan Eckels <eveckels@gmail.com>
2026-03-27 18:41:50 -0400

    fix wording

--- src/types/CardCreditsTypes.ts
+++ src/types/CardCreditsTypes.ts
@@ -501,7 +501,7 @@ export type ComponentDisabledDisplayNameType = typeof COMPONENT_DISABLED_DISPLAY
  */
 export const CREDIT_SUMMARY_SECTIONS = {
     MONTHLY_CREDITS: {
-        displayName: 'Monthly credits',
+        displayName: 'Monthly Credits',
         icon: 'calendar-days'
     },
     CURRENT_CREDITS: {
